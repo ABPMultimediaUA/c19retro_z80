d3d8e876afe969e74ca01835eaaf132c532e3bbf
Carlos Arismendi <ceas1@alu.ua.es>
2020-10-27 21:53:50 +0100

    heart feo de carlos

---
+++ src/assets/sprites/heart.h
@@ -0,0 +1,10 @@
+// Data created with Img2CPC - (c) Retroworks - 2007-2017
+#ifndef _SRC_ASSETS_IMG_HEART_HEART_H_
+#define _SRC_ASSETS_IMG_HEART_HEART_H_
+
+#include <types.h>
+#define SP_HEART_W 4
+#define SP_HEART_H 16
+extern const u8 sp_heart[4 * 16];
+
+#endif
